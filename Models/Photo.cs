f46c7b2f886a2e145a15cf77ca40b7aa720e1633
nichlls <107227159+nichlls@users.noreply.github.com>
2025-09-03 15:46:34 +0100

    Initial

---
+++ Models/Photo.cs
@@ -0,0 +1,9 @@
+namespace UnsplashCLI.Models;
+
+public class Photo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Author { get; set; }
+    public string? ImageURL { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.EntityFrameworkCore;
+using UnsplashCLI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PhotoDb>(option =>
+    option.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"))
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "hello");
+
+app.MapGet(
+    "/photos",
+    async (PhotoDb db) =>
+    {
+        var photos = await db.Photos.ToListAsync();
+
+        return photos;
+    }
+);
+app.MapPut(
+    "/photo",
+    async (PhotoDb db, Photo photo) =>
+    {
+        await db.AddAsync(photo);
+        await db.SaveChangesAsync();
+
+        return Results.Ok($"Uploaded: {photo}");
+    }
+);
+
+app.Run();
+
+// Setup database context
+public class PhotoDb : DbContext
+{
+    public PhotoDb(DbContextOptions<PhotoDb> options)
+        : base(options) { }
+
+    public DbSet<Photo> Photos { get; set; }
+}
